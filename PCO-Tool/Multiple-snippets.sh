d991501989540d6abdd97a8799ead166804a22bc
Jesper V Nielsen <31649178+jespervnielsen@users.noreply.github.com>
2018-08-16 12:41:13 +0200

    Update Multiple-snippets.sh

--- PCO-Tool/Multiple-snippets.sh
+++ PCO-Tool/Multiple-snippets.sh
@@ -1,2 +1,7 @@
 #Clear all subdomains, on site
 pco -e prod purge <app> --subdomain="*" --regex="*"
+
+Clear multiple individual subdomains. <app is the top domain eg: [example].com and not the app name, as you might expect
+pco -e prod purge <app> --subdomain="aeroe" --regex="robots.txt" && \
+pco -e prod purge <app> --subdomain="alleroed" --regex="robots.txt" && \
+pco -e prod purge <app>--subdomain="assens" --regex="robots.txt" && \
