6a58f0c439ad8e6b4e9fb38a67d74ec306048eee
Jesper V Nielsen <31649178+jespervnielsen@users.noreply.github.com>
2018-09-05 08:59:05 +0200

    Update Multiple-snippets.sh

--- PCO-Tool/Multiple-snippets.sh
+++ PCO-Tool/Multiple-snippets.sh
@@ -5,3 +5,9 @@ pco -e prod purge <app> --subdomain="*" --regex="*"
 pco -e prod purge <app> --subdomain="aeroe" --regex="robots.txt" && \
 pco -e prod purge <app> --subdomain="alleroed" --regex="robots.txt" && \
 pco -e prod purge <app>--subdomain="assens" --regex="robots.txt" && \
+
+#Check task wp_cron logs
+pco logs <app> task wp_cron -e prod
+
+#Check error log
+pco logs <app> error -e prod
