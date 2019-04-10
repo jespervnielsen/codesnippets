a58186398634cca6769cb825ff61962fa116d117
Jesper V Nielsen <31649178+jespervnielsen@users.noreply.github.com>
2019-04-10 08:50:52 +0200

    Update ftp-download.sh

--- Bash/ftp-download.sh
+++ Bash/ftp-download.sh
@@ -1,2 +1,5 @@
+//use your windows ubuntu, then go to correct drive/folder
+cd /mnt/d/
+
 wget -r --user="username" --password="password" ftp://example.com/ -m
 //-m is for --mirror, to mirror the entire ftp location
