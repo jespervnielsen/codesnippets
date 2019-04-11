e76f3c31f67622e69f5dd5abc7685accf6721e29
Jesper V Nielsen <31649178+jespervnielsen@users.noreply.github.com>
2019-04-11 14:02:24 +0200

    Update ftp-download.sh

--- Bash/ftp-download.sh
+++ Bash/ftp-download.sh
@@ -1,5 +1,13 @@
 //use your windows ubuntu, then go to correct drive/folder
 cd /mnt/d/
 
+//ftp download
 wget -r --user="username" --password="password" ftp://example.com/ -m
 //-m is for --mirror, to mirror the entire ftp location
+
+//rsync - by ftp, or ssh. - requires rsync in install on remote server - usefull to only download files that not exists local
+rsync -r -a -v -e "ssh" --delete username@x.x.x.x:/var/www/domainpath /dump
+
+//ssh
+scp -r username@x.x.x.x:/var/www/domainpath .
+(. for current folder)
