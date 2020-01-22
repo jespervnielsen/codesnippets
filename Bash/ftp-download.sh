3d3a67fe978ae099295a9e79f5f24099f3d70e4b
Jesper V Nielsen <31649178+jespervnielsen@users.noreply.github.com>
2020-01-22 08:20:14 +0100

    Update ftp-download.sh

--- Bash/ftp-download.sh
+++ Bash/ftp-download.sh
@@ -7,8 +7,11 @@ wget -r --user="username" --password="password" ftp://example.com/ -m
 
 //rsync - by ftp, or ssh. - requires rsync in install on remote server - usefull to only download files that not exists local
 //-e "ssh for ssh. 
+// @link https://linux.die.net/man/1/rsync
 // --delete is for local delete, if there is files on local, that exists, but not on server.
 rsync -r -a -v -e "ssh" --delete username@x.x.x.x:/var/www/domainpath ./dump
+//ftp, and with a bandwithlimit on 1500kb, since a lot of hosts has trouble with connection, if too high
+rsync -r -a -v --bwlimit=1500 --progress username@x.x.x.x:/var/www/domainpath ./dump
 
 //ssh
 scp -r username@x.x.x.x:/var/www/domainpath .
