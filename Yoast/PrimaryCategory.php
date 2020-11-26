b4e945e8c245ed3b75fd9339640e34bb89982381
Jesper V Nielsen <31649178+jespervnielsen@users.noreply.github.com>
2020-11-26 12:55:47 +0100

    Update PrimaryCategory.php

--- Yoast/PrimaryCategory.php
+++ Yoast/PrimaryCategory.php
@@ -1,25 +1,32 @@
-	public function get_primary_category( $post = null ) {
-		$post = get_post( $post );
 
-		$terms = (array) get_the_category( $post->ID );
+/**
+ * Find the primary term. - if no primary is found use first found.
+ *
+ * @param mixed $post
+ * @return object
+ */
+public function get_primary_category( $post = null ) : object {
+	$post = get_post( $post );
 
-		$yoast_primary_category_id = get_post_meta( $post->ID, '_yoast_wpseo_primary_category', true );
+	$terms = (array) get_the_category( $post->ID );
 
-		// Find if primary term is part post terms.
-		foreach ( $terms as $term ) {
-				if ( $term->term_id == $yoast_primary_category_id ) {
-					return $term;
-				}
-		}
+	$yoast_primary_category_id = get_post_meta( $post->ID, '_yoast_wpseo_primary_category', true );
 
-		// Primary category not found. return first term;
-		foreach ( $terms as $term ) {
+	// Find if primary term is part post terms.
+	foreach ( $terms as $term ) {
+		if ( $term->term_id == $yoast_primary_category_id ) {
 			return $term;
 		}
-
-		// Terms was empty. - can happen after migrations, and programmatically creation of posts.
-		// Find "default" term
-		$default_category = get_option('default_category');
-		return get_term( $default_category );
-
 	}
+
+	// Primary category not found. return first term;
+	foreach ( $terms as $term ) {
+		return $term;
+	}
+
+	// Terms was empty. - can happen after migrations, and programmatically creation of posts.
+	// Find "default" term
+	$default_category = get_option( 'default_category' );
+	return get_term( $default_category );
+
+}
