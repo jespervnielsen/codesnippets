97dcf23834453901ee675d4c1b6d30099dd0af56
Jesper V Nielsen <31649178+jespervnielsen@users.noreply.github.com>
2020-11-26 11:43:23 +0100

    Create PrimaryCategory.php

---
+++ Yoast/PrimaryCategory.php
@@ -0,0 +1,25 @@
+	public function get_primary_category( $post = null ) {
+		$post = get_post( $post );
+
+		$terms = (array) get_the_category( $post->ID );
+
+		$yoast_primary_category_id = get_post_meta( $post->ID, '_yoast_wpseo_primary_category', true );
+
+		// Find if primary term is part post terms.
+		foreach ( $terms as $term ) {
+				if ( $term->term_id == $yoast_primary_category_id ) {
+					return $term;
+				}
+		}
+
+		// Primary category not found. return first term;
+		foreach ( $terms as $term ) {
+			return $term;
+		}
+
+		// Terms was empty. - can happen after migrations, and programmatically creation of posts.
+		// Find "default" term
+		$default_category = get_option('default_category');
+		return get_term( $default_category );
+
+	}
